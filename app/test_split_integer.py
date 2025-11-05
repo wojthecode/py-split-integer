305b16c885282acb5cd85a2b9c596fbcb83e8c84
Wojciech Witkowski <witus85@gmail.com>
2025-11-05 17:58:10 +0100

    Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -3,12 +3,12 @@ from app.split_integer import split_integer
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     assert sum(split_integer(10, 3)) == 10
-    assert sum(split_integer(17, 4)) == 17
+    assert sum(split_integer(29, 4)) == 29
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert len(set(split_integer(40, 5))) == 1
-    assert len(set(split_integer(6, 2))) == 1
+    assert split_integer(40, 5) == [8, 8, 8, 8, 8]
+    assert split_integer(6, 2) == [3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
@@ -19,6 +19,7 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     assert split_integer(27, 4) == [6, 7, 7, 7]
     assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
+    assert split_integer(17, 4) == [4, 4, 4, 5]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
